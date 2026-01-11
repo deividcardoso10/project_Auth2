5c209cfba62a767cdd5112e3a4ebe7850a82d5ab
unknown <deivid.cardoso.100@gmail.com>
2026-01-10 22:38:52 -0300

    testing authorization

--- src/main/java/com/devsuperior/demo/services/UserService.java
+++ src/main/java/com/devsuperior/demo/services/UserService.java
@@ -7,9 +7,11 @@ import com.devsuperior.demo.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements UserDetailsService {
 
     private UserRepository repository;
